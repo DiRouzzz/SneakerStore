51de3f3717f1b49b56aea0fb5ba6ad41065ad9d4
Oleg Portnov <drose1@list.ru>
2025-03-24 21:00:04 +0400

    upd context 4

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -1,22 +1,31 @@
 import { useContext } from 'react';
 import { AppContext } from '../../context';
+import { Link } from 'react-router-dom';
 
 export const Info = ({ title, description, src, alt, width, height, setIsOrderComplete }) => {
 	const { setCartOpened } = useContext(AppContext);
 	return (
 		<div className='cartEmpty d-flex align-center justify-center flex-column flex'>
-			<img className='mb-20' width={width} height={height} src={src} alt={alt} />
+			<img
+				className='mb-20'
+				width={width}
+				height={height}
+				src={src}
+				alt={alt}
+			/>
 			<h2>{title}</h2>
 			<p className='opacity-6'>{description}</p>
-			<button
-				onClick={() => {
-					setCartOpened(false);
-					// setIsOrderComplete(false);
-				}}
-				className='greenButton'>
-				<img src='/img/arrow.svg' alt='arrow' />
-				Вернуться назад
-			</button>
+			<Link to='/'>
+				<button
+					onClick={() => {
+						setCartOpened(false);
+						// setIsOrderComplete(false);
+					}}
+					className='greenButton'>
+					<img src='/img/arrow.svg' alt='arrow' />
+					Вернуться назад
+				</button>
+			</Link>
 		</div>
 	);
 };
